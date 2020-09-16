d6637192162e7eb06dc7eaf3270d8a065ba61cb6
Shawn <shawn@wowa.ca>
2020-09-16 17:20:48 -0400

    classes Q

---
+++ Classes/Aclasstomanageacollectionofobjects.py
@@ -0,0 +1,44 @@
+# DO NOT USE THE WORD input ANYWHERE!
+
+# We provide class Rectangle and starter code for class RectangleCollection.
+
+# Complete class RectangleCollection, which has one list instance variable, rectangles, that should initially refer to an empty list. Write a method add_rectangle that takes a Rectangle as a parameter and appends it to the rectangles list. Write a method get_same_area_rects that takes a number as a parameter and returns a list of all Rectangles from the rectangles list that have that area.
+
+
+from typing import List
+
+class Rectangle:
+    """ A rectangle with a width and height. """
+
+    def __init__(self, w: int, h: int) -> None:
+        # """Create a new rectangle of width w and height h.
+
+        # >>> r = Rectangle(1, 2)
+        # >>> r.width
+        # 1
+        # >>> r.height
+        # 2
+        # """
+
+        self.width = w
+        self.height = h
+
+    def get_area(self) -> int:
+        # """Return the area of this rectangle.
+
+        # >>> r = Rectangle(10, 20)
+        # >>> r.get_area()
+        # 200
+        # """
+
+        return self.width * self.height
+
+
+class RectangleCollection:
+
+    def __init__(self) -> None:
+        # """
+        # >>> rc = RectangleCollection()
+        # >>> rc.rectangles
+        # []
+        # """
---
+++ Classes/SpecialList.py
@@ -0,0 +1,57 @@
+# SpecialList is a class that has two instance variables value_list and size. value_list refers to a list, and size stores an integer representing the maximum number of items that can be stored in the list.
+
+
+
+class SpecialList:
+    # """A list that can hold a limited number of items."""
+
+    def __init__(self, size: int) -> None:
+        # """Initialize this special list to hold at most size items.
+
+        # >>> L = SpecialList(10)
+        # >>> L.size
+        # 10
+        # >>> L.value_list
+        # []
+        # """
+        # # complete this code
+
+
+    def push_value(self, new_value: object) -> None:
+        # """Append new_value to this list, if there is enough space in the list
+        # according to its maximum size. If there is insufficient space, new_value
+        # should not be added to the list.
+
+        # >>> L = SpecialList(2)
+        # >>> L.push_value(3)
+        # >>> L.push_value(4)
+        # >>> L.push_value(5)
+        # >>> L.value_list
+        # [3, 4]
+        # """
+        # # complete this code
+
+
+    def pop_most_recent_value(self) -> object:
+        # """Return the value added most recently to value_list and remove it from
+        # the list.
+
+        # Precondition: len(self.value_list) != 0
+
+        # >>> L = SpecialList(10)
+        # >>> L.push_value(3)
+        # >>> L.push_value(4)
+        # >>> L.value_list
+        # [3, 4]
+        # >>> L.pop_most_recent_value()
+        # 4
+        # """
+        # # complete this code
+
+
+    def compare(self, other: "SpecialList") -> int:
+        # """Return 0 if both SpecialList objects contain the same number
+        # of items. Return 1 if self contains more items than other.
+        # Return -1 if self contains fewer items than other.
+        # """
+        # # complete this code
