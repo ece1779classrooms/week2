6470400caa3bc336f0e52495d26fd0c672e4065d
Shawn <shawn@wowa.ca>
2020-09-16 17:16:09 -0400

    conditionsandif

---
+++ ConditionalsAndLoops/Passwordvalidity.py
@@ -0,0 +1,8 @@
+def check_password(passwd: str) -> bool:
+    # """A strong password has a length greater than or equal to 6, contains at
+    # least one lowercase letter, at least one uppercase letter, and at least
+    # one digit.  Return True iff passwd is considered strong.
+
+    # >>> check_password('I<3csc108')
+    # True
+    # """
---
+++ ConditionalsAndLoops/ReversePhoneLookup(Part2).py
@@ -0,0 +1,20 @@
+# You have been asked to write a function that supplies the "reverse lookup" name for the person associated with a phone number.
+# This means that given a phone number, you get the name of the person who is registered with that phone number. For this problem, you have been asked to write a solution using a dictionary
+
+
+from typing import Dict
+
+def reverse_lookup_dictionary(phone_num: str,
+                              phone_to_name: Dict[str, str]) -> str:
+    # """This function receives a phone number phone_num, and a dictionary
+    # phone_to_name in which each key is a phone number and each value
+    # is the name associated with that phone number.
+
+    # Return the name associated with phone_num in phone_to_name, or
+    # an empty string if there is no match.
+
+    # >>> reverse_lookup_dictionary("416-555-3498", {"416-555-3498": \
+    #     "John A. Macdonald", "647-555-9812": "Louis Riel", "416-555-6543": \
+    #     "Canoe Head", "905-555-6681":"Tim Horton"})
+    # 'John A. Macdonald'
+    # """
